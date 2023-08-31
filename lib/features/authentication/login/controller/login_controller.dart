cd6f90f3bc3d8c8a33708099239fee6386672b45
oscar onyenacho <oscardre8@gmail.com>
2023-08-31 15:23:14 +0100

    added signin validation

--- lib/features/authentication/login/controller/login_controller.dart
+++ lib/features/authentication/login/controller/login_controller.dart
@@ -1,5 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
   RxBool isVisible = true.obs;
+
+  var passwordController = TextEditingController();
+  var emailController = TextEditingController();
+
+  clearError(RxString val) => val.value = '';
+  RxString emailErrorText = ''.obs;
+
+  RxString passwordErrorText = ''.obs;
+
+  validateInput() {
+    if (!emailController.text.trim().isEmail) {
+      emailErrorText.value = 'Incorrect email format';
+    } else if (emailController.text.trim().isEmpty) {
+      emailErrorText.value = 'Please enter your email';
+    } else if (passwordController.text.trim().isEmpty) {
+      passwordErrorText.value = 'Please enter your password';
+    }
+  }
 }
